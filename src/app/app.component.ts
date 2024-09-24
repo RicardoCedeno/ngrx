2434c02e08c8876f537631ecb6f6ba09754846f4
Ricardo Jose Cedeño Mancilla <sjaimes16@outlook.com>
2024-09-24 17:21:29 -0500

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { user } from './models/User';
 import { signalState } from '@ngrx/signals';
 import { userState } from './models/UserState';
 import { UserServiceService } from './services/user-service.service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -17,11 +18,41 @@ export class AppComponent {
  
 
   userService = inject(UserServiceService)
+  http = inject(HttpClient)
 
   ngOnInit(){
-    this.userService.getUserState();
-    this.userService.updateUserAdminStatus(true);
-    this.userService.updateUser({username: 'cic2', password:'123456'})
-    this.userService.updateUserAndAdminStatus({username: 'cic3', password:'123456'}, false)
+    // this.userService.getUserState();
+    // this.userService.updateUserAdminStatus(true);
+    // this.userService.updateUser({username: 'cic2', password:'123456'})
+    // this.userService.updateUserAndAdminStatus({username: 'cic3', password:'123456'}, false)
+    this.getData()
+    this.getDataAsync();
   }
+
+  constructor(){}
+
+
+  getData(): void {
+    this.http.get('https://dog.ceo/api/breeds/image/random').toPromise()
+      .then(response => {
+        console.log('no async ',response);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+      console.log("prueba")
+  }
+
+  async getDataAsync(): Promise<void> {
+    try {
+      const response = await this.http.get('https://dog.ceo/api/breeds/image/random').toPromise();
+      console.log('async ', response);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+    console.log("prueba")
+  }
+
+  
+
 }
